3dc27228b236184bed9962ca340bb2b850e983af
Eric Charnesky <eric.charnesky@careevolution.com>
2018-01-18 18:32:37 -0500

    Hi Eric!

--- Week2Examples/Week2Examples/Program.cs
+++ Week2Examples/Week2Examples/Program.cs
@@ -11,6 +11,7 @@ namespace Week2Examples
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hi there Eric!");
             Console.ReadKey();
         }
     }
