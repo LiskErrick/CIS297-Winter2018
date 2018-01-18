2f4f174c705d87617b388e067fe7ce6d57012bbe
Eric Charnesky <eric.charnesky@careevolution.com>
2018-01-18 18:56:32 -0500

    examples from Chapter 6

--- Week2Examples/Week2Examples/Program.cs
+++ Week2Examples/Week2Examples/Program.cs
@@ -8,11 +8,45 @@ namespace Week2Examples
 {
     class Program
     {
+        
+        public static int myFavoriteNumber, yourFavoriteNumber;
+
+        public string firstName;
+
+      
+        // defaults to private if you don't declare
+        int thirteen = 0b00001101;
+
+        int seventeen = 0x11;
+
+        static int someBigValue = 123_456_789;
+
         static void Main(string[] args)
         {
+            var areYouHungry = true;
+
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hi there Eric!");
+
+            // write will not add a new line at the end
+            Console.Write(myFavoriteNumber);
+
+            // escape sequence for newline
+            Console.Write("\n");
+
+            // always print a new line regardless
+            Console.WriteLine();
+
+            Console.WriteLine(someBigValue);
+
+            Console.Write("Next line please!");
+
+            // read key will wait for any key press
             Console.ReadKey();
+
+            //Console.ReadLine(); will wait for the enter key
         }
+
+        private String _name;
     }
 }
