60115eeb07bae6239701284dbc860c3ad2455df5
Eric Charnesky <eric.charnesky@careevolution.com>
2018-01-25 18:56:55 -0500

    Math Tutor App

---
+++ MyFirstWIndowsForm/MyFirstWIndowsForm/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace MyFirstWIndowsForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.clickMeButton = new System.Windows.Forms.Button();
+            this.questionLabel = new System.Windows.Forms.Label();
+            this.answerBox = new System.Windows.Forms.TextBox();
+            this.rightOrWrongLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // clickMeButton
+            // 
+            this.clickMeButton.Location = new System.Drawing.Point(1048, 277);
+            this.clickMeButton.Name = "clickMeButton";
+            this.clickMeButton.Size = new System.Drawing.Size(393, 243);
+            this.clickMeButton.TabIndex = 0;
+            this.clickMeButton.Text = "Answer";
+            this.clickMeButton.UseVisualStyleBackColor = true;
+            this.clickMeButton.Click += new System.EventHandler(this.clickMeButton_Click);
+            // 
+            // questionLabel
+            // 
+            this.questionLabel.AutoSize = true;
+            this.questionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.questionLabel.Location = new System.Drawing.Point(277, 357);
+            this.questionLabel.Name = "questionLabel";
+            this.questionLabel.Size = new System.Drawing.Size(237, 63);
+            this.questionLabel.TabIndex = 1;
+            this.questionLabel.Text = "1 + 1 = ?";
+            // 
+            // answerBox
+            // 
+            this.answerBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.answerBox.Location = new System.Drawing.Point(562, 354);
+            this.answerBox.Name = "answerBox";
+            this.answerBox.Size = new System.Drawing.Size(374, 68);
+            this.answerBox.TabIndex = 2;
+            this.answerBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.answerBox_KeyPress);
+            // 
+            // rightOrWrongLabel
+            // 
+            this.rightOrWrongLabel.AutoSize = true;
+            this.rightOrWrongLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rightOrWrongLabel.Location = new System.Drawing.Point(627, 126);
+            this.rightOrWrongLabel.Name = "rightOrWrongLabel";
+            this.rightOrWrongLabel.Size = new System.Drawing.Size(0, 63);
+            this.rightOrWrongLabel.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1862, 1088);
+            this.Controls.Add(this.rightOrWrongLabel);
+            this.Controls.Add(this.answerBox);
+            this.Controls.Add(this.questionLabel);
+            this.Controls.Add(this.clickMeButton);
+            this.Name = "Form1";
+            this.Text = "My First App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button clickMeButton;
+        private System.Windows.Forms.Label questionLabel;
+        private System.Windows.Forms.TextBox answerBox;
+        private System.Windows.Forms.Label rightOrWrongLabel;
+    }
+}
+
---
+++ MyFirstWIndowsForm/MyFirstWIndowsForm/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyFirstWIndowsForm
+{
+    public partial class Form1 : Form
+    {
+        private int result;
+        private Random random;
+        public Form1()
+        {
+            InitializeComponent();
+            result = 2;
+            random = new Random();
+        }
+
+        private void clickMeButton_Click(object sender, EventArgs e)
+        {
+            HandleButtonOrEnter();
+        }
+
+        private void HandleButtonOrEnter()
+        {
+            string answer = answerBox.Text;
+
+            int answerAsInt = Int32.Parse(answer);
+
+            if (answerAsInt == result)
+            {
+                rightOrWrongLabel.Text = "Correct!";
+                int firstNumber = random.Next(20);
+                int secondNumber = random.Next(20);
+                result = firstNumber + secondNumber;
+                questionLabel.Text = $"{firstNumber} + {secondNumber}"
+                    + " = ?";
+
+                // could also do
+                //questionLabel.Text = firstNumber.ToString() + " + " + secondNumber.ToString()
+                //    + " = ?";
+
+                // blank out the answer box
+                answerBox.Text = string.Empty;
+
+                // brings the cursor back to the answerBox so I can type
+                answerBox.Focus();
+            }
+            else
+            {
+                rightOrWrongLabel.Text = "Wrong! Try again!";
+            }
+        }
+
+        private void answerBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // https://stackoverflow.com/questions/3558814/net-textbox-handling-the-enter-key
+            if (e.KeyChar == (char)Keys.Return)
+            {
+                HandleButtonOrEnter();
+            }
+        }
+    }
+}
